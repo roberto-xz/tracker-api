f4f1fb1009bab91fb126b710ddb3cd296d9e1f8b
roberto-xz <devv-robert@outlook.com>
2025-02-15 15:24:59 -0300

    add: Adicionado entidade/model Package Closes #7

---
+++ src/main/java/com/rdtech/tracker_api/entity/PackageIntity.java
@@ -0,0 +1,194 @@
+
+package com.rdtech.tracker_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *****
+ * @date 15/02/2015 14:60 pm
+ * @author roberto-xz
+ *****
+ */
+
+@Entity
+@Table(name = "Package") // Define o nome da tabela no banco de dados
+public class PackageIntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Double packageId;
+    
+    private Double containerID = -1d; // Indica que não pertence a um container
+    
+    @Column(nullable = false)
+    private Double statusID;     // Status do pacote, em preparação, entransito, entregue, aguardando transport..
+    
+    @Column(nullable = false)
+    private String trackerCode;  // Codigo de Rastreio
+    
+    @Column(nullable = false)
+    private String userUUID;
+    private String description;
+  
+    @Column(nullable = false)
+    private String stateOrigem;  // Estado de Origem, exp: Pernambuco, São Paulo, Goiais, etc..
+    @Column(nullable = false)
+    private String cityOrigem;   // cidade de Origem
+    @Column(nullable = false)
+    private String stateDestine;
+    @Column(nullable = false)
+    private String cityDestine;
+    
+    @Column(nullable = false)
+    private String streetAdress; // Rua
+    
+    @Column(nullable = false)
+    private String neighborhood; // Bairro
+    
+    @Column(nullable = false)
+    private String homeNumber;   // Número da casa
+    
+    @Column(nullable = false)
+    private Object dateStartTransport;   // Data do início do transport
+    
+    @Column(nullable = false)
+    private Object dateEndTransport;     // Data da entrega
+    
+    @Column(nullable = false)
+    private Object expectedDeliveryDate; // Data prevista para entrega
+
+    public Double getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(Double packageId) {
+        this.packageId = packageId;
+    }
+
+    public Double getContainerID() {
+        return containerID;
+    }
+
+    public void setContainerID(Double containerID) {
+        this.containerID = containerID;
+    }
+
+    public Double getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(Double statusID) {
+        this.statusID = statusID;
+    }
+
+    public String getTrackerCode() {
+        return trackerCode;
+    }
+
+    public void setTrackerCode(String trackerCode) {
+        this.trackerCode = trackerCode;
+    }
+
+    public String getUserUUID() {
+        return userUUID;
+    }
+
+    public void setUserUUID(String userUUID) {
+        this.userUUID = userUUID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStateOrigem() {
+        return stateOrigem;
+    }
+
+    public void setStateOrigem(String stateOrigem) {
+        this.stateOrigem = stateOrigem;
+    }
+
+    public String getCityOrigem() {
+        return cityOrigem;
+    }
+
+    public void setCityOrigem(String cityOrigem) {
+        this.cityOrigem = cityOrigem;
+    }
+
+    public String getStateDestine() {
+        return stateDestine;
+    }
+
+    public void setStateDestine(String stateDestine) {
+        this.stateDestine = stateDestine;
+    }
+
+    public String getCityDestine() {
+        return cityDestine;
+    }
+
+    public void setCityDestine(String cityDestine) {
+        this.cityDestine = cityDestine;
+    }
+
+    public String getStreetAdress() {
+        return streetAdress;
+    }
+
+    public void setStreetAdress(String streetAdress) {
+        this.streetAdress = streetAdress;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public void setHomeNumber(String homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public Object getDateStartTransport() {
+        return dateStartTransport;
+    }
+
+    public void setDateStartTransport(Object dateStartTransport) {
+        this.dateStartTransport = dateStartTransport;
+    }
+
+    public Object getDateEndTransport() {
+        return dateEndTransport;
+    }
+
+    public void setDateEndTransport(Object dateEndTransport) {
+        this.dateEndTransport = dateEndTransport;
+    }
+
+    public Object getExpectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public void setExpectedDeliveryDate(Object expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+    
+}
+
+
