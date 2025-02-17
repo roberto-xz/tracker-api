e0590746c20b96a4ca02010350ddae917566f978
roberto-xz <devv-robert@outlook.com>
2025-02-17 12:35:26 -0300

    fixe: corrigido nome dos pacotes package para packages, pois há conflitos ao utilizar um pocote com o nome package

--- src/main/java/com/rdtech/tracker_api/event/package/PackageCreatedEvent.java
+++
@@ -1,5 +0,0 @@
-package com.rdtech.tracker_api.event.package;
-
-public class PackageCreatedEvent {
-
-}
--- src/main/java/com/rdtech/tracker_api/observer/package/AfterCreatedPackageObserver.java
+++
@@ -1,5 +0,0 @@
-package com.rdtech.tracker_api.observer.package;
-
-public class AfterCreatedPackageObserver {
-
-}
