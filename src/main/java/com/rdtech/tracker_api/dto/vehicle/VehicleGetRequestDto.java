350a0296886889028e44e526710fd02be64949e4
roberto-xz <devv-robert@outlook.com>
2025-02-19 20:38:53 -0300

    add: Request e Reponse DTO para recuperação de veículos, Closes #9

--- src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleGetRequestDto.java
+++ src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleGetRequestDto.java
@@ -1,5 +1,18 @@
+
 package com.rdtech.tracker_api.dto.vehicle;
 
+/**
+ *****
+ * @date 19/02/2025
+ * @author roberto-xz
+ *****
+**/
 public class VehicleGetRequestDto {
-
+    private Long vehicleId;
+    private String vehiclePlate;
+    
+    public Long getVehicleId() {return vehicleId;}
+    public void setVehicleId(Long vehicleId) {this.vehicleId = vehicleId;}
+    public String getVehiclePlate() {return vehiclePlate;}
+    public void setVehiclePlate(String vehiclePlate) {this.vehiclePlate = vehiclePlate;}
 }
