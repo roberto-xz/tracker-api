a36e190fc72bc25bc999a0fa8262d9375f9be6be
roberto-xz <devv-robert@outlook.com>
2025-02-19 20:33:15 -0300

    add: Request e Reponse DTO para remoção de veículos

--- src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleDeletedResponseDto.java
+++ src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleDeletedResponseDto.java
@@ -1,5 +1,24 @@
+
 package com.rdtech.tracker_api.dto.vehicle;
 
+/**
+ *****
+ * @date 19/02/2025
+ * @author roberto-xz
+ *****
+**/
 public class VehicleDeletedResponseDto {
-
+    private String messagem;
+    private Integer statusCode;
+    
+    public VehicleDeletedResponseDto(){}
+    
+    public VehicleDeletedResponseDto(String messagem, Integer statusCode) {
+        this.messagem = messagem;
+        this.statusCode = statusCode;
+    }
+    public String getMessagem() {return messagem;}
+    public void setMessagem(String messagem) {this.messagem = messagem;}
+    public Integer getStatusCode() {return statusCode;}
+    public void setStatusCode(Integer statusCode) {this.statusCode = statusCode;}
 }
