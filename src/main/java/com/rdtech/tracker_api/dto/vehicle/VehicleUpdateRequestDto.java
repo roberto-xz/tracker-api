90a688ca080a761dd7b7b17f97ed48b10745032f
roberto-xz <devv-robert@outlook.com>
2025-02-19 20:28:40 -0300

    add: Request e Reponse DTO para atualização de veículos

--- src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleUpdateRequestDto.java
+++ src/main/java/com/rdtech/tracker_api/dto/vehicle/VehicleUpdateRequestDto.java
@@ -1,5 +1,28 @@
+
 package com.rdtech.tracker_api.dto.vehicle;
 
+/**
+ *****
+ * @date 19/02/2025
+ * @author roberto-xz
+ *****
+**/
 public class VehicleUpdateRequestDto {
-
+    private Long driverID;
+    private Boolean vehicleAsProblem;
+    private Integer vehicleProblemLevel;
+    
+    public Long getDriverID() {return driverID;}
+    public void setDriverID(Long driverID) {this.driverID = driverID;}
+    public Boolean getVehicleAsProblem() {return vehicleAsProblem;}
+    
+    public void setVehicleAsProblem(Boolean vehicleAsProblem) { 
+        this.vehicleAsProblem = vehicleAsProblem;
+    }
+    
+    public Integer getVehicleProblemLevel() {return vehicleProblemLevel;}
+    
+    public void setVehicleProblemLevel(Integer vehicleProblemLevel) {
+        this.vehicleProblemLevel = vehicleProblemLevel;
+    }
 }
