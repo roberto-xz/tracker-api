2f24e14abcbc50eb1a6bd6e85c6b92dffb9dc966
Juciara Xavier <juciara.xavier@aluno.ifsertao-pe.edu.br>
2025-02-20 19:51:22 -0300

    add: adicionado DTOs for Status Closes #33

--- src/main/java/com/rdtech/tracker_api/dto/status/StatusCreatedResponseDto.java
+++ src/main/java/com/rdtech/tracker_api/dto/status/StatusCreatedResponseDto.java
@@ -1,5 +1,24 @@
 package com.rdtech.tracker_api.dto.status;
 
-public class StatusCreatedResponseDto {
+/**
+ *
+ * @date 20/02/2025
+ * @author Juci-x
+ *
+ **/
 
+public class StatusCreatedResponseDto {
+    private Long statusId;
+    private String Menssagem;
+
+    public StatusCreatedResponseDto() {}
+    public StatusCreatedResponseDto(Long statusId, String menssagem) {
+        this.statusId = statusId;
+        Menssagem = menssagem;
+    }
+
+    public Long getStatusId() {return statusId;}
+    public void setStatusId(Long statusId) {this.statusId = statusId;}
+    public String getMenssagem() {return Menssagem;}
+    public void setMenssagem(String menssagem) {Menssagem = menssagem;}
 }
--- src/main/java/com/rdtech/tracker_api/dto/status/StatusDeletedResponseDto.java
+++ src/main/java/com/rdtech/tracker_api/dto/status/StatusDeletedResponseDto.java
@@ -1,5 +1,24 @@
 package com.rdtech.tracker_api.dto.status;
 
-public class StatusDeletedResponseDto {
+/**
+ *
+ * @date 20/02/2025
+ * @author Juci-x
+ *
+ **/
 
+public class StatusDeletedResponseDto {
+    private Long statusId;
+    private String messagem;
+
+    public StatusDeletedResponseDto() {}
+    public StatusDeletedResponseDto(Long statusId, String messagem) {
+        this.statusId = statusId;
+        this.messagem = messagem;
+    }
+
+    public Long getStatusId() {return statusId;}
+    public void setStatusId(Long statusId) {this.statusId = statusId;}
+    public String getMessagem() {return messagem;}
+    public void setMessagem(String messagem) {this.messagem = messagem;}
 }
