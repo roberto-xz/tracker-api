b6bc21eb26da56db0e0d7951ff929f4d34e870c4
roberto-xz <devv-robert@outlook.com>
2025-02-21 13:32:07 -0300

    add: Adicionado o repository para o model Driver

---
+++ src/main/java/com/rdtech/tracker_api/repository/DriverRepository.java
@@ -0,0 +1,11 @@
+package com.rdtech.tracker_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rdtech.tracker_api.entity.Driver;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver,Long>{
+    boolean existsByDriverRegisterCode(String driverRegisterCode);
+}
