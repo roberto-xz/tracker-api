b80a8f973eade84568e1e217d7dd300eaa7b59b6
roberto-xz <devv-robert@outlook.com>
2025-02-24 13:43:17 -0300

    fixe: Adicionado notação @RequestBody no controller

--- src/main/java/com/rdtech/tracker_api/controller/driver/SetDriverInVehicleController.java
+++ src/main/java/com/rdtech/tracker_api/controller/driver/SetDriverInVehicleController.java
@@ -2,6 +2,7 @@ package com.rdtech.tracker_api.controller.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,8 @@ public class SetDriverInVehicleController {
     SetDriverInVehicleService setDVService;
     
     @PutMapping("/vehicle")
-    public SetDriverInVehicleResponseDto run(SetDriverInVehicleRequestDto req){
+    public SetDriverInVehicleResponseDto run(@RequestBody SetDriverInVehicleRequestDto req){
         SetDriverInVehicleResponseDto res = this.setDVService.run(req);
         return res;   
-    }
+    } 
 }
