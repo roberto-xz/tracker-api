1f4db4ac29e0784a38013eff790b29b1ae668c00
roberto-xz <devv-robert@outlook.com>
2025-02-23 21:52:19 -0300

    fixe: corrigido criação de um status para cada pacote criado.

--- src/main/java/com/rdtech/tracker_api/observer/packages/AfterCreatedPackageObserver.java
+++ src/main/java/com/rdtech/tracker_api/observer/packages/AfterCreatedPackageObserver.java
@@ -4,7 +4,6 @@ package com.rdtech.tracker_api.observer.packages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
 import com.rdtech.tracker_api.entity.ContainerEntity;
 import com.rdtech.tracker_api.entity.HistoricOfPackageEntity;
 import com.rdtech.tracker_api.entity.PackageEntity;
@@ -15,6 +14,7 @@ import com.rdtech.tracker_api.repository.HistoricOfPackageRepository;
 import com.rdtech.tracker_api.repository.PackageRepository;
 import com.rdtech.tracker_api.repository.StatusRepository;
 import com.rdtech.tracker_api.util.CreateTrackerCode;
+
 /**
  *****
  * @date 23/02/2025
@@ -33,6 +33,9 @@ public class AfterCreatedPackageObserver {
     private final StatusRepository     statusRepository;
     private final HistoricOfPackageRepository historicOfPackageRepository;
 
+    private final String packageStatus  = "Aguardando Transporte";
+    private final String containeStatus = "Aguardando Pacotes";
+
     @Autowired // Injeção de dependencias via construtor
     public AfterCreatedPackageObserver(
         PackageRepository p, 
@@ -50,65 +53,70 @@ public class AfterCreatedPackageObserver {
     public void run(PackageCreatedEvent event) {
         this.packageEntity = event.getPackage();
         
-        if (this.setExistentContainer()) {
-            this.createHistoric();
-            return;
+        // verifica se há um container, caso não , cria um novo
+        if (!this.setExistentContainer()) this.createNewContainer();
+         
+        // atualiza o status do pacote
+        if ( !this.statusRepository.existsByStatusText(this.packageStatus)) {
+            Status status = new Status(this.packageStatus);
+            Status statusCreated = statusRepository.save(status); 
+            this.packageEntity.setStatusId(statusCreated.getStatusID());
+
+        }else {
+            Status tempStatus = this.statusRepository.getByStatusText(this.packageStatus);
+            this.packageEntity.setStatusId(tempStatus.getStatusID());
+        }
+        
+        // atualiza o status do container
+        if ( !this.statusRepository.existsByStatusText(this.containeStatus)) {
+            Status status = new Status(this.containeStatus);
+            
+            Status statusCreated = statusRepository.save(status);
+            this.containerEntity.setStatusID(statusCreated.getStatusID());
+        }else {
+            Status tempStatus = this.statusRepository.getByStatusText(this.containeStatus);
+            this.containerEntity.setStatusID(tempStatus.getStatusID());
         }
 
-        this.createNewContainer();
+        this.packageRepository.save(this.packageEntity);
+        this.containerRepository.save(this.containerEntity);
         this.createHistoric();
-        return;
     }
     
-    // Se já existe um container que vai para o mesmo 
-    // local do pacote.
+    
     private boolean setExistentContainer() {
         this.containerEntity = this.containerRepository.getByStateDestine(this.packageEntity.getStateDestine());
         
-        if (this.containerEntity != null) {
-            Status status = new Status("Aguardando Transporte");
-            Status statusCreated = statusRepository.save(status);
-
+        if (this.containerEntity != null) {   
             this.packageEntity.setContainerId(this.containerEntity.getContainerId());
-            this.packageEntity.setStatusId(statusCreated.getStatusID());
             this.packageEntity.setTrackerCode(new CreateTrackerCode.Builder().build().generate());
-            this.packageRepository.save(this.packageEntity);
+
+            // atualiza o número de pacotes no container
+            Integer numPackages = this.containerEntity.getNumPackages();
+            this.containerEntity.setNumPackages(numPackages+1);
             return true;
         }
         return false;
     }
 
-    // cria um novo container para o pacote
     private void createNewContainer() {
         this.containerEntity = new ContainerEntity();
         this.containerEntity.setVehicleId(-1L); // o veículo ainda não existe
         this.containerEntity.setCityDestine(this.packageEntity.getCityDestine());
         this.containerEntity.setStateDestine(this.packageEntity.getCityDestine());
+        this.containerEntity.setStatusID(-1L);
         this.containerEntity.setDateStartTransport("00-00-00 00:00 PM");
         this.containerEntity.setDateEndTransport("00-00-00 00:00 PM");
         this.containerEntity.setMaxPackages(120);
         this.containerEntity.setNumPackages(1);
 
-        // criando status
-        Status statusContainer = new Status("Aguardando Pacotes");
-        Status statusPackage   = new Status("Aguardando Transporte");
-        
-        // atribuindo os status
-        this.packageEntity.setStatusId(this.statusRepository.save(statusPackage).getStatusID());
-        this.containerEntity.setStatusID(this.statusRepository.save(statusContainer).getStatusID());
-
-
-        // salvando o novo container
+        // precisa salvar para obter ID, e atribuir ao pacote
         ContainerEntity newContainer = this.containerRepository.save(this.containerEntity);
          
-        // atribuido relações é código de rastreio
-         this.packageEntity.setContainerId(newContainer.getContainerId());
-         this.packageEntity.setTrackerCode(new CreateTrackerCode.Builder().build().generate());
-
-        this.packageRepository.save(this.packageEntity);
+        this.packageEntity.setContainerId(newContainer.getContainerId());
+        this.packageEntity.setTrackerCode(new CreateTrackerCode.Builder().build().generate());
     }
     
-    // criar o historico de mudaças de container
     private void createHistoric() {
         this.packageHistoric = new HistoricOfPackageEntity();
         this.packageHistoric.setPackageId(this.packageEntity.getPackageId());
