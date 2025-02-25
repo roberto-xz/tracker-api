bf0b4b4675dc2a7c7e777424bf6867da893ba5b7
roberto-xz <devv-robert@outlook.com>
2025-02-25 08:36:04 -0300

    add: adicionado rota para mudar um container de veículo

---
+++ src/main/java/com/rdtech/tracker_api/service/container/ChangeContainerVehicleService.java
@@ -0,0 +1,68 @@
+
+package com.rdtech.tracker_api.service.container;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rdtech.tracker_api.entity.ContainerEntity;
+import com.rdtech.tracker_api.entity.Status;
+import com.rdtech.tracker_api.entity.VehicleEntity;
+import com.rdtech.tracker_api.repository.ContainerRepository;
+import com.rdtech.tracker_api.repository.StatusRepository;
+import com.rdtech.tracker_api.repository.VehicleRepository;
+
+@Service
+public class ChangeContainerVehicleService {
+    private final ContainerRepository container;
+    private final VehicleRepository   vehicle;
+    private final StatusRepository    status;
+    
+    @Autowired
+    public ChangeContainerVehicleService(
+        ContainerRepository container, 
+        VehicleRepository vehicle,
+        StatusRepository status
+    ) {
+        this.container = container;
+        this.vehicle = vehicle;
+        this.status = status;
+    }
+
+    public Object run(Long containerId, Long vehicleId) {
+        Optional<ContainerEntity> container = this.container.findById(containerId);
+        Optional<VehicleEntity>    vehicle  = this.vehicle.findById(vehicleId);
+
+        if (!container.isPresent()){
+            Map<String,Object> response = Map.of("message", "Container não encontrado","StatusCode", 404);
+            return response;
+        }
+
+        if (!vehicle.isPresent()){
+            Map<String,Object> response = Map.of("message", "Veículo não encontrado","StatusCode", 404);
+            return response;
+        }
+
+        if (vehicle.isPresent() && vehicle.get().getIsAvailable()){
+            Map<String,Object> response = Map.of("message", "Esse veículo já está sendo usado","StatusCode", 404);
+            return response;
+        }
+
+        container.get().setContainerId(vehicleId);
+        if (this.status.existsByStatusText("Esperando Transport")){
+            Long status = this.status.getByStatusText("Esperando Transport").getStatusID();
+            container.get().setStatusID(status);
+        }else {
+            Status status = new Status("Esperando Transport");
+            Long newStatus = this.status.save(status).getStatusID();
+            container.get().setStatusID(newStatus);
+        }
+
+        this.container.save(container.get());
+        Map<String,Object> response = Map.of("message", "O veículo do container foi atualizado com sucesso", "StatusCode",200);
+        return response;
+    }
+ 
+}
