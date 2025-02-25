070a6b4b290352d83b97a8585225d4fdbf832eab
Juciara Xavier <juciara.xavier@aluno.ifsertao-pe.edu.br>
2025-02-25 10:09:02 -0300

    add: Service - ReportVehicleProblemService.

--- src/main/java/com/rdtech/tracker_api/service/vehicle/ReportVehicleProblemService.java
+++ src/main/java/com/rdtech/tracker_api/service/vehicle/ReportVehicleProblemService.java
@@ -1,7 +1,9 @@
 
 package com.rdtech.tracker_api.service.vehicle;
 import java.util.Map;
+import java.util.Optional;
 
+import com.rdtech.tracker_api.entity.VehicleEntity;
 import org.springframework.stereotype.Service;
 
 import com.rdtech.tracker_api.repository.VehicleRepository;
@@ -16,8 +18,24 @@ public class ReportVehicleProblemService {
     }
 
 
-    public Object run(long vehicleId, Integer problemLevel){
-        Map<String,Object> response = Map.of("message", "Veículo não encontrado","StatusCode", 404);
-        return response;
+    public Map<String,Object> run(long vehicleId, Integer problemLevel){
+        //busca o veiculo pelo Id
+        Optional<VehicleEntity> optionalVehicle = vehicle.findById(vehicleId);
+
+        //verifica se o veiculo foi encontrado
+        if(optionalVehicle.isEmpty()){
+            return Map.of("message", "Veículo não encontrado","StatusCode", 404);
+        }
+
+        //Atualiza os campos veiculo
+        VehicleEntity vehicleEntity = optionalVehicle.get();
+        vehicleEntity.setVehicleAsProblem(true);
+        vehicleEntity.setVehicleProblemLevel(problemLevel);
+
+        //salva as alterações
+        vehicle.save(vehicleEntity);
+
+        //retorna mensagem de sucesso
+        return Map.of("message", "Problema reportado com sucesso","StatusCode", 200);
     }
 }
